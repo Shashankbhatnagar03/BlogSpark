065d6d648c1325b0a0a2118a7bf8d82a8009c1e5
Shashankbhatnagar03 <Shashank.bhatnagar3@gmail.com>
2024-08-17 19:47:53 +0530

    Created Auth pages and Implemented its functionality

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,21 @@
+const Quote = () => {
+  return (
+    <>
+      <div className="bg-slate-200 h-screen flex justify-center flex-col  ">
+        <div className="flex justify-center">
+          <div className=" max-w-lg  ">
+            <div className="text-3xl font-bold ">
+              "There is no greater agony than bearing an untold story inside
+              you."
+            </div>
+            <div className=" text-xl font-medium	text-slate-600	mt-3">
+              --Maya Angelou
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Quote;
